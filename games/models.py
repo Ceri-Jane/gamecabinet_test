59fbb65a139dfcc560921db7fc528373ca1f500e
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-05 12:34:42 +0000

    add game model and register in admin

--- games/models.py
+++ games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    
+    min_players = models.IntegerField()
+    max_players = models.IntegerField()
+
+    year_published = models.IntegerField(blank=True, null=True)
+
+    image_url = models.URLField(blank=True, null=True)
+
+    bgg_id = models.CharField(max_length=50, blank=True, null=True, help_text="BoardGameGeek Game ID")
+
+    def __str__(self):
+        return self.title
